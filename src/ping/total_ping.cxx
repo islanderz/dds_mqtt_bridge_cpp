c158ace22b431e8f4a01ab4899804c7336acd603
islanderz <kamilns@hotmail.com>
2017-03-25 04:14:30 +0200

    =TODOs and some logfiles

--- src/ping/total_ping.cxx
+++ src/ping/total_ping.cxx
@@ -6,14 +6,15 @@
 #include <string>
 #include <sys/time.h>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 using namespace rti;
 
-#define WIFI_500    0
-#define WIFI_2K     1
-#define DDS_500     2
-#define DDS_2K      3
+#define WIFI_500    1 //TODO Actually need to be the total DDS_500+WIFI_500 - see TODO below (value can be kept and overwritten for now)
+#define WIFI_2K     3 //TODO Actually need to be the total DDS_2K+WIFI_2K - see TODO below (value can be kept and overwritten for now)
+#define DDS_500     0
+#define DDS_2K      2
 #define STAT_LEN    4
 
 
@@ -27,6 +28,10 @@ public:
         boost::unique_lock<boost::mutex> scoped_lock(
             io_mutex);
         values[stat_pos] = value;
+    }
+
+    double get_value(int stat_pos) {
+        return values[stat_pos];
     }
     
     void get_values(double *output_values) {
@@ -42,10 +47,11 @@ private:
     PingStats* ps;    
     DdsConnection* conn;
 public:
+    // Receiver for DDS messages
     WifiPingerReceiver(PingStats* ps) : ps(ps) {
         conn = new DdsConnection(this, 200, "wifi-ping-stats", 128);
     }
-
+    // Writing the WiFi ping values.
     virtual void sink(char* data, int len, int msgId, bool isLast) {
         double* data2 = (double*)data;
         ps->write_stat(WIFI_500, data2[0]);
@@ -74,6 +80,7 @@ private:
     boost::mutex ps_mutex;
     boost::condition_variable ps_cond;
 
+    // Compute DDS Rount Trip Trime
     void ping_step(int stat_pos, char *buff, int buff_size,
         IDataSink* ping_sink) {
         boost::unique_lock<boost::mutex> lock(ps_mutex);
@@ -86,9 +93,16 @@ private:
 
         while (ping_count != ping_rec)
             ps_cond.wait(lock);
-
+	
+	// Computing & Wriing DDS RTT
         double tt = GET_TDIFF(call_time, resp_time);
-        ps->write_stat(stat_pos, tt);
+        ps->write_stat(stat_pos, tt*1000);
+        // TODO P1 - Need to overwrite the value of values[WIFI_500] (which is same as values[stat_pos+1]) with Wifi+DDS delay
+        // TODO P1 - Need to overwrite the value of values[WIFI_2K] (which is  also values[stat_pos+1]) with wifi+DDS delay
+        // TODO P1 Summary - Caculate total value as values[stat_pos+1] + tt*1000
+        // TODO P2(For sunday/nextweek) - Smoothen out total here (commented in responsder) afer adding all
+        // TODO P2(For sunday/nextweek) - Send to ROS the smoothed out  total
+
         usleep(200000);
     }
 
@@ -110,6 +124,11 @@ public:
             buff500[i] = (char)(i % 255);
         for (int i = 0; i < 20000; i++)
             buff2k[i] = (char)(i % 255);
+    }
+
+
+    ~DdsPinger() {
+	
     }
 
     void start() {
@@ -146,22 +165,44 @@ int main(int argc, char **argv) {
     ros::NodeHandle nodeHandle;
 
     PingStats ps;
-    
+    std::stringstream delays;    
     WifiPingerReceiver wpr(&ps);
     DdsPinger dds(&ps);
+    std::ofstream log_file_dds_ping;
 
     // need to allow dds subscriptions to settle
     sleep(1);
     
     dds.start();
-    
+ 	   
     double stats[STAT_LEN];
+	std::stringstream ss;
+	time_t rawtime;
+	struct tm * timeinfo;
+	char buffer[80];
+	time (&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	strftime(buffer,80,"%d_%m_%Y_%H_%M_%S",timeinfo);
+	std::string date_time(buffer);
+
+	ss << "ping_dds_wifi_" << date_time << ".txt";
+	log_file_dds_ping.open(ss.str(), std::ofstream::out | std::ofstream::app);
     while (1) {
         sleep(1);
+        delays.str( std::string() );
+        delays.clear();
         ps.get_values(stats);
-        for (int i = 0; i < STAT_LEN; i++)
+
+        for (int i = 0; i < STAT_LEN; i++) {
             cerr << fixed << stats[i] << " ";
-        cerr << endl;
+            delays << fixed << stats[i] << " ";
+         }
+      cerr << endl;
+      log_file_dds_ping << fixed << delays.str() << endl;
     }
+
+    log_file_dds_ping.close();
+   
     return 0;
 }
